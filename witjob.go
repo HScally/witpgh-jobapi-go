a895ce0fd801069b4052a7cdfe651a9a21bb6ab0
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-07-18 11:43:25 -0700

    Update to match correct main file

--- witjob.go
+++ witjob.go
@@ -2,39 +2,44 @@ package main
 // go mod init
 // go mod tidy
 // go mod vendor
-import (
+mport (
 	"log"
 	"net/http"
 	"os"
 	"runtime"
+	"witpgh-jobapi-go/app/shared/database"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
+	// Verbose logging with file name and line number
 	log.SetFlags(log.Lshortfile)
-	
+
+	// Use all CPU cores
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
-	
+
 	database.ConnectWITJobBoard()
 
 	port := os.Getenv("PORT")
 
 	if len(port) == 0 {
 		port = "3000"
-	}	
-	http.ListenAndServe(":" + port, nil)
+	}
+
+	http.ListenAndServe(":"+port, nil)
+
 }
 
-func configureEnviornemnts() {
+func configureEnvironments() {
 	os.Clearenv()
 
 	err := godotenv.Load("doc.env")
 	if err != nil {
 		log.Println(err)
 		log.Fatal("Error loading .env file")
-}
+	}
 }
