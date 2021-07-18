96c057fcc1b23c2ba6f5a8f16729ca3f379666df
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-07-18 11:47:29 -0700

    Update database connection

--- app/shared/database/database.go
+++ app/shared/database/database.go
@@ -7,15 +7,18 @@ import (
 
 	_ "github.com/lib/pq"
 )
+
 var WITPGH *sql.DB
 
 func ConnectWITJobBoard() {
 	var err error
 
+	// Connect to PostgreSQL
 	if WITPGH, err = sql.Open("postgres", getConnection()); err != nil {
-		log.Println("SQL Driver Error, err)
+		log.Println("SQL Driver Error", err)
 	}
 
+	// Check if is alive
 	if err = WITPGH.Ping(); err != nil {
 		log.Println("WIT Job Board Database Error", err)
 	}
