e19a4f16a506692408e7351a800feb94a4d8483c
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-07-21 18:14:33 -0700

    Add sample api structure

--- app/shared/repositories/accountmanagement.go
+++ app/shared/repositories/accountmanagement.go
@@ -1,14 +1,17 @@
-package accountmanagement
+package repositories
 
-type Employer struct {
-	Id int
-	PublicId string
-	EmployerKey string
-	Status int
-	Email string
-	Password string
-	MustResetPassword int
-	Firstname string
-	Lastname string
-	SendMobileNotices int
+import (
+	"witpgh-jobapi-go/app/shared/database"
+	"witpgh-jobapi-go/app/shared/repositories/accountmanagement"
+)
+
+type RepositoryRegistry struct {
+}
+
+func NewRepositoryRegistry() *RepositoryRegistry {
+	return &RepositoryRegistry{}
+}
+
+func (registry *RepositoryRegistry) GetEmployerAccountRepository() *accountmanagement.AccountRepository {
+	return accountmanagement.NewAccountRepository(database.WITPGH)
 }
