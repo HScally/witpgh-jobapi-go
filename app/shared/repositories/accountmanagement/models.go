630afbe32b6cfe318fe27cc802826ccb137521ed
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-07-21 19:04:17 -0700

    Add all db models

--- app/shared/repositories/accountmanagement/models.go
+++ app/shared/repositories/accountmanagement/models.go
@@ -1,5 +1,42 @@
 package accountmanagement
 
+
+type EmployerCompanies struct {
+	Id					int    `json:"id"`	
+	PublicId			string `json:"public_id"`
+	EmployerId			int    `json:"employer_id"`
+	Status				bool   `json:"status"`
+	CompanyName			string `json:"company_name"`
+	CompanyStatment		string `json:"company_statement"`
+	LogoImageFileName	string `json:"logo_image_file_name"`
+	LogoObjectFileName	string `json:"logo_object_file_name"`
+	CompanyLogo			string `json:"company_logo"`	
+	HasLogo				bool   `json:"has_logo"`
+	CompanyUrl			string `json:"company_url"`
+	CompanyDescription	string `json:"company_description"`
+	AddressLine1		string `json:"address_line1"`
+	AddressLine2		string `json:"address_line2"`
+	City				string `json:"city"`
+	State				string `json:"state"`
+	Zipcode				string `json:"zipcode"`
+	Country				string `json:"country"`
+	Latitude			int    `json:latitude`
+	Longitude			int	   `json:"longitude"`
+}
+
+type EmployerJob struct {
+	Id                    int    `json:"id"`
+	PublicId			  int    `json:"public_id"`
+	EmployerId            int    `json:"employer_id"`
+	EmployerCompanyId     int    `json:"employer_company_id"`
+	IsClosed			  bool   `json:"is_closed"`
+	JobTitle			  string `json:"job_title"`
+	CompanyHq			  string `json:"company_hq"`
+	JobTypeId			  int    `json:"job_type_id"`
+	RegionalRestrictionId int    `json:"regional_restriction_id"`
+	JobDescription		  string `json:"job_description"`
+}
+
 type Employer struct {
 	Id                int    `json:"id"`
 	PublicId          string `json:"public_id"`
@@ -11,4 +48,16 @@ type Employer struct {
 	Firstname         string `json:"firstname"`
 	Lastname          string `json:"lastname"`
 	SendMobileNotices int    `json:"send_mobile_notices"`
+}
+
+type JobRegionalRestrictions struct {
+	Id			  int 	 `json:"id"`
+	Description   string `json:"description"`
+	RestrictionId int    `json:"restriction_id"`
+}
+
+type JobTypes struct {
+	Id			int    `json:"id"`
+	Description string `json:"description"`
+	TypeId		int    `json:"type_id"`
 }
