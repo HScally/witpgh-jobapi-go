e9b9788bf4c682a52d3d1675f4a05c2f91f16f1d
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-08-01 14:17:39 -0700

    Add jobmanagement repo

--- app/shared/repositories/repositoryregistry.go
+++ app/shared/repositories/repositoryregistry.go
@@ -4,6 +4,7 @@ package repositories
 import (
 	"witpgh-jobapi-go/app/shared/database"
 	"witpgh-jobapi-go/app/shared/repositories/accountmanagement"
+	"witpgh-jobapi-go/app/shared/repositories/jobmanagement"
 )
 
 type RepositoryRegistry struct {
@@ -15,4 +16,8 @@ func NewRepositoryRegistry() *RepositoryRegistry {
 
 func (registry *RepositoryRegistry) GetEmployerAccountRepository() *accountmanagement.AccountRepository {
 	return accountmanagement.NewAccountRepository(database.WITPGH)
+}
+
+func (registry *RepositoryRegistry) GetEmployerJobRepository() *jobmanagement.EmployerJobRepository {
+	return jobmanagement.NewJobRepository(database.WITPGH)
 }
