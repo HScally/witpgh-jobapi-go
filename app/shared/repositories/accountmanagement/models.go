406108d9aafcf6c6b88c20e8abfaf93d4bf962c1
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-08-01 15:39:07 -0700

    Removed unneeded models and move EmployerCompanies to new file

--- app/shared/repositories/accountmanagement/models.go
+++ app/shared/repositories/accountmanagement/models.go
@@ -1,28 +1,5 @@
 package accountmanagement
 
-type EmployerCompanies struct {
-	Id					int    `json:"id"`	
-	PublicId			string `json:"public_id"`
-	EmployerId			int    `json:"employer_id"`
-	Status				bool   `json:"status"`
-	CompanyName			string `json:"company_name"`
-	CompanyStatment		string `json:"company_statement"`
-	LogoImageFileName	string `json:"logo_image_file_name"`
-	LogoObjectFileName	string `json:"logo_object_file_name"`
-	CompanyLogo			string `json:"company_logo"`	
-	HasLogo				bool   `json:"has_logo"`
-	CompanyUrl			string `json:"company_url"`
-	CompanyDescription	string `json:"company_description"`
-	AddressLine1		string `json:"address_line1"`
-	AddressLine2		string `json:"address_line2"`
-	City				string `json:"city"`
-	State				string `json:"state"`
-	Zipcode				string `json:"zipcode"`
-	Country				string `json:"country"`
-	Latitude			int    `json:latitude`
-	Longitude			int	   `json:"longitude"`
-}
-
 type Employer struct {
 	Id                int    `json:"id"`
 	PublicId          string `json:"public_id"`
@@ -34,16 +11,4 @@ type Employer struct {
 	Firstname         string `json:"firstname"`
 	Lastname          string `json:"lastname"`
 	SendMobileNotices int    `json:"send_mobile_notices"`
-}
-
-type JobRegionalRestrictions struct {
-	Id			  int 	 `json:"id"`
-	Description   string `json:"description"`
-	RestrictionId int    `json:"restriction_id"`
-}
-
-type JobTypes struct {
-	Id			int    `json:"id"`
-	Description string `json:"description"`
-	TypeId		int    `json:"type_id"`
 }
