5cad8833e2c4d6293ef20a01fe635546b8c8b676
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-08-01 16:16:36 -0700

    Change struct variables from boolean to int

--- app/shared/repositories/companymanagement/models.go
+++ app/shared/repositories/companymanagement/models.go
@@ -4,13 +4,13 @@ type EmployerCompanies struct {
 	Id					int    `json:"id"`
 	PublicId			string `json:"public_id"`
 	EmployerId			int    `json:"employer_id"`
-	Status				bool   `json:"status"`
+	Status				int    `json:"status"`
 	CompanyName			string `json:"company_name"`
 	CompanyStatment		string `json:"company_statement"`
 	LogoImageFileName	string `json:"logo_image_file_name"`
 	LogoObjectFileName	string `json:"logo_object_file_name"`
 	CompanyLogo			string `json:"company_logo"`
-	HasLogo				bool   `json:"has_logo"`
+	HasLogo				int    `json:"has_logo"`
 	CompanyUrl			string `json:"company_url"`
 	CompanyDescription	string `json:"company_description"`
 	AddressLine1		string `json:"address_line1"`
