a686c9f46692ff764567e4333ece976b2dc03b84
Jane Schmitz <jschmitz@navigatingcancer.com>
2021-08-01 15:39:48 -0700

    Add model file to compnaymanagement package

---
+++ app/shared/repositories/companymanagement/models.go
@@ -0,0 +1,24 @@
+package companymanagement
+
+type EmployerCompanies struct {
+	Id					int    `json:"id"`
+	PublicId			string `json:"public_id"`
+	EmployerId			int    `json:"employer_id"`
+	Status				bool   `json:"status"`
+	CompanyName			string `json:"company_name"`
+	CompanyStatment		string `json:"company_statement"`
+	LogoImageFileName	string `json:"logo_image_file_name"`
+	LogoObjectFileName	string `json:"logo_object_file_name"`
+	CompanyLogo			string `json:"company_logo"`
+	HasLogo				bool   `json:"has_logo"`
+	CompanyUrl			string `json:"company_url"`
+	CompanyDescription	string `json:"company_description"`
+	AddressLine1		string `json:"address_line1"`
+	AddressLine2		string `json:"address_line2"`
+	City				string `json:"city"`
+	State				string `json:"state"`
+	Zipcode				string `json:"zipcode"`
+	Country				string `json:"country"`
+	Latitude			int    `json:latitude`
+	Longitude			int	   `json:"longitude"`
+}
